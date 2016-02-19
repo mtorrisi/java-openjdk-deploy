5b6b75a2d265e118f597fbec8aa0f53fbc5a1f1e
mtorrisi <torrisi.mario@gmail.com>
2016-02-19 18:11:23 +0100

    First test just download and unzip REPAST libs.

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@
 # of CODE-RADE here by deploying pre-packaged binary files.
 
 # Build only downloads and unpacks the code to the install dir.
-. /etc/profile.d/modules.sh
+#. /etc/profile.d/modules.sh
 
 echo "build number is ${BUILD_NUMBER}"
-SOURCE_FILE=${NAME}-${VERSION}-linux-x64.tar.gz
-module load ci
-module add jdk/${JAVA_VERSION}
+SOURCE_FILE=${NAME}_${VERSION}_linux-x64.zip
+#module load ci
+#module add jdk/${JAVA_VERSION}
 mkdir -p ${SRC_DIR}
-echo "getting the file from Oracle"
+echo "Getting REPAST Libraries from http://grid.ct.infn.it/csgf/binaries/repast/${VERSION}"
 
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
 # claim the download
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
-  wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/${VERSION}-b${BUILD}/${SOURCE_FILE}" -O ${SRC_DIR}/${SOURCE_FILE}
+  wget "http://grid.ct.infn.it/csgf/binaries/repast/${VERSION}/${SOURCE_FILE}" -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
@@ -30,6 +30,7 @@ fi
 mkdir -p ${WORKSPACE}/${NAME}-${VERSION}
 # the name of the tar upack dir is some wierd combination of underscores and numbers, so we'll just redefined where
 # the code gets unpacked
-tar xfz ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}/${NAME}-${VERSION} --skip-old-files --strip-components=1
+unzip ${SRC_DIR}/${SOURCE_FILE} -d ${WORKSPACE}/${NAME}-${VERSION}
 cd ${WORKSPACE}/${NAME}-${VERSION}
+ls -al
 # TODO - see if REPAST builds.
