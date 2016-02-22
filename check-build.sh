1f6c3aab04237b56a2529b2cff1764092dfa5dec
mtorrisi <torrisi.mario@gmail.com>
2016-02-22 16:21:10 +0100

    Added clean module dir, added -u param in unzip command. Fixed #9 #10.

--- check-build.sh
+++ check-build.sh
@@ -36,12 +36,12 @@ $PLUGINS/saf.core.ui_$VERSION/lib/*:\
 echo "[check-build.sh] - Executing test..."
 java -cp $CLASSPATH repast.simphony.runtime.RepastBatchMain -help
 
+# clean out previous module
+rm -rf modules ${LIBRARIES_MODULES}/${NAME}
 
 # Create module
 echo "[check-build.sh] - Making module"
-if [ ! ! -d module  ]; then
   mkdir -p modules
-fi
 (
 cat <<MODULE_FILE
 #%Module1.0
