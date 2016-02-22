64d448a023654a5461de9b205e30404682dff3fe
mtorrisi <torrisi.mario@gmail.com>
2016-02-22 19:25:43 +0100

    Deleted duplicate cp destination folder folder in deploy.sh. Fixed #22.

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ module add deploy
 
 echo "[deploy.sh] - Now deploying to ${SOFT_DIR}"
 cd ${WORKSPACE}/${NAME}-${VERSION}
-echo "[deploy.sh] -  REPAST $VERSION will now go into ${SOFT_DIR}/"
+echo "[deploy.sh] - REPAST $VERSION will now go into ${SOFT_DIR}/"
 mkdir -p ${SOFT_DIR}
-cp -r ${WORKSPACE}/${NAME}-${VERSION} ${SOFT_DIR}${SOFT_DIR}
+cp -r ${WORKSPACE}/${NAME}-${VERSION} ${SOFT_DIR}
 #unzip -u ${SRC_DIR}/${SOURCE_FILE} -d ${SOFT_DIR}
 
 # REPAST specific costants
